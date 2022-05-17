4000df1ba2c23abc4753a05e3168f7a26a4a3b4f
vanes <vanes@LAPTOP-DG4J0EB8>
2022-05-17 13:53:32 -0300

    atualização

--- AulasAlexJDev/src/CursoJava/Thread/TelaTimeThread.java
+++ AulasAlexJDev/src/CursoJava/Thread/TelaTimeThread.java
@@ -1,10 +1,17 @@
 package CursoJava.Thread;
 
 import javax.swing.JDialog;
-import javax.swing.plaf.DimensionUIResource;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
+import java.awt.GridBagConstraints;
+import javax.swing.plaf.DimensionUIResource;
+import java.awt.GridBagLayout;
 public class TelaTimeThread extends JDialog{
 	
+	private JPanel jpanel = new JPanel(new GridBagLayout());
+	
+	private JLabel descricaoHora = new JLabel("Time Thread 1");
 	
 	
 	public TelaTimeThread() {
@@ -12,11 +19,19 @@ public class TelaTimeThread extends JDialog{
 		setSize(new DimensionUIResource(240, 240) );
 		setLocationRelativeTo(null);
 		setResizable(false);
+		
+		GridBagConstraints gridBagConstraints = new GridBagConstraints();// controlar posicionamento de componentes
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 0;
+		
+		descricaoHora.setPreferredSize(new DimensionUIResource(WIDTH, HEIGHT));
+		jpanel.a
 		//primeira parte concluida
-		
-		
-		
 		setVisible(true);
+		
+		
+		
+		
 	}
 
 }
