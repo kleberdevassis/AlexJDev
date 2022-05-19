b3b1188bd726b31baaf0006cbaada69e120a6ebd
vanes <vanes@LAPTOP-DG4J0EB8>
2022-05-19 16:31:09 -0300

    Adicionando no Thread o JPanel o Label e o TextFild

--- AulasAlexJDev/src/CursoJava/Thread/TelaTimeThread.java
+++ AulasAlexJDev/src/CursoJava/Thread/TelaTimeThread.java
@@ -3,15 +3,20 @@ package CursoJava.Thread;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
+import java.awt.*;
+import javax.swing.*;
 import java.awt.GridBagConstraints;
 import javax.swing.plaf.DimensionUIResource;
 import java.awt.GridBagLayout;
 public class TelaTimeThread extends JDialog{
 	
-	private JPanel jpanel = new JPanel(new GridBagLayout());
+	private JPanel jpanel = new JPanel(new GridBagLayout());//Painel de componentes
 	
 	private JLabel descricaoHora = new JLabel("Time Thread 1");
+	private JTextField mostaTempo = new JTextField();
+	
+	private JLabel descricaoHora2 = new JLabel("Time Thread 2");
+	private JTextField mostaTempo2 = new JTextField();
 	
 	
 	public TelaTimeThread() {
@@ -24,9 +29,22 @@ public class TelaTimeThread extends JDialog{
 		gridBagConstraints.gridx = 0;
 		gridBagConstraints.gridy = 0;
 		
-		descricaoHora.setPreferredSize(new DimensionUIResource(WIDTH, HEIGHT));
-		jpanel.a
-		//primeira parte concluida
+		descricaoHora.setPreferredSize(new Dimension(200, 25));
+		jpanel.add(descricaoHora, gridBagConstraints);
+		
+		mostaTempo.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy++;
+		jpanel.add(mostaTempo, gridBagConstraints);
+		
+		descricaoHora2.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy++;
+		jpanel.add(descricaoHora2, gridBagConstraints);
+		
+		mostaTempo2.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy++;
+		jpanel.add(mostaTempo2, gridBagConstraints);
+		
+		add(jpanel, BorderLayout.WEST);
 		setVisible(true);
 		
 		
